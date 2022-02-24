9e2d61c5248a20c1971670bef2e66f7f1f578de9
suhailyazdan <yazdan.suhail@gmail.com>
2022-02-24 21:16:49 +0530

    changes in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h2>React on Github pages</h2>
+      <p>Lorem ipsum dolor sit amet</p>
       <p>Lorem ipsum dolor sit amet</p>
     </div>
   );
